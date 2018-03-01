8486e2a3a9c46404330e283bc2986a8b16c9edde
Farhan Chauhan <farhan687@gmail.com>
2018-03-01 09:29:59 +0530

    Serve build as static directory

--- server/index.js
+++ server/index.js
@@ -2,15 +2,18 @@ const express = require('express');
 const cors = require('cors');
 const products = require('./data/products');
 
+const PORT = process.env.PORT || 5000;
+
 const app = express();
 app.use(cors());
+app.use(express.static('build'));
 
 /* GET: products
   Query Params: offset, length
   offset: starting id point
   length: number of products in response
 */
-app.get('/products', (req, res) => {
+app.get('/api/v1/products', (req, res) => {
   let { offset = 0, length = 10 } = req.query;
   offset = parseInt(offset, 10);
   length = parseInt(length, 10);
@@ -20,5 +23,5 @@ app.get('/products', (req, res) => {
   });
 });
 
-app.listen(8011, () => console.log('Cart test App listening on port 8011!'));
+app.listen(PORT, () => console.log(`Cart test App listening on port ${PORT}!`));
 
