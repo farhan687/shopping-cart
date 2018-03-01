85dffec4cc553bbcb7ea4f4bb3206eb8e1e3b2a5
Farhan Chauhan <farhan687@gmail.com>
2018-03-01 09:30:16 +0530

    Use BASE_URL env variable for local dev

--- src/helper/dataService.js
+++ src/helper/dataService.js
@@ -1,11 +1,14 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:8011';
+if (process.env.REACT_APP_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
+}
+const productsUrl = '/api/v1/products';
 
 export default {
   fetchProducts(offset = 0, length = 10) {
     return new Promise((resolve, reject) => {
-      axios.get(`/products?offset=${offset}&length=${length}`).then(({ data }) => {
+      axios.get(`${productsUrl}?offset=${offset}&length=${length}`).then(({ data }) => {
         resolve(data);
       }).catch(reject);
     });
